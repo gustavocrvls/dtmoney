5acb951b616f576bf65a4ccb935f6e967d414d5d
Gustavo Carvalho Silva <gustavocrvl42@gmail.com>
2021-05-24 19:31:24 -0300

    Estilização dos botões de Entrada e Saída

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,10 @@
 import Modal from 'react-modal';
-import { Container } from './styles';
+
+import closeImg from '../../assets/close.svg';
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
+
+import { Container, TransactionTypeContainer } from './styles';
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -14,6 +19,13 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
       overlayClassName="react-modal-overlay"
       className="react-modal-content"
     >
+      <button
+        type="button"
+        className="react-modal-close"
+        onClick={onRequestClose}>
+        <img src={closeImg} alt="Close"
+      />
+      </button>
       <Container>
         <h2>Cadastrar Transação</h2>
 
@@ -24,6 +36,18 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
           placeholder="Valor"
           type="number"
         />
+
+        <TransactionTypeContainer>
+          <button type="button">
+            <img src={incomeImg} alt="Entrada"></img>
+            <span>Entrada</span>
+          </button>
+          <button type="button">
+            <img src={outcomeImg} alt="Saída"></img>
+            <span>Saída</span>
+          </button>
+        </TransactionTypeContainer>
+
         <input
           placeholder="Categoria"
         />
