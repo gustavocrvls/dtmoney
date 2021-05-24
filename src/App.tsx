70991b4f3268ecca67e4709cd61e757f0bc558b9
Gustavo Carvalho Silva <gustavocrvl42@gmail.com>
2021-05-24 12:21:03 -0300

    Adição do parametro de root no modal

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ import Modal from 'react-modal';
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 
-import { GlobalStyle } from "./styles/global"
+import { GlobalStyle } from "./styles/global";
+
+Modal.setAppElement('#root');
 
 export function App() {
   const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
