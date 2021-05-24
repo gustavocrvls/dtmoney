838a02148ab04f1257b835e708137aa836ba3235
Gustavo Carvalho Silva <gustavocrvl42@gmail.com>
2021-05-24 19:54:50 -0300

    Funcionamento do form de nova transação

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { FormEvent, useState } from 'react';
 import Modal from 'react-modal';
 
 import closeImg from '../../assets/close.svg';
@@ -13,7 +13,14 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
+  const [title, setTitle] = useState('');
+  const [value, setValue] = useState(0);
+  const [category, setCategory] = useState('');
   const [type, setType] = useState('deposit');
+
+  function handleCreateNewTransaction(event: FormEvent) {
+    event.preventDefault();
+  }
 
   return (
     <Modal
@@ -29,15 +36,19 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
         <img src={closeImg} alt="Close"
         />
       </button>
-      <Container>
+      <Container onSubmit={handleCreateNewTransaction}>
         <h2>Cadastrar Transação</h2>
 
         <input
           placeholder="Título"
+          value={title}
+          onChange={e => setTitle(e.target.value)}
         />
         <input
           placeholder="Valor"
           type="number"
+          value={value}
+          onChange={e => setValue(Number(e.target.value))}
         />
 
         <TransactionTypeContainer>
@@ -63,6 +74,8 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
 
         <input
           placeholder="Categoria"
+          value={category}
+          onChange={e => setCategory(e.target.value)}
         />
         <button type="submit">Cadastrar</button>
       </Container>
