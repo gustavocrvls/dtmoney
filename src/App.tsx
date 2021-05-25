c8c900c13ba5dc293ed52ad32ef2e9db86015ca7
Gustavo Carvalho Silva <gustavocrvl42@gmail.com>
2021-05-24 21:47:54 -0300

    Criação de Contexto de Transactions

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { Header } from "./components/Header";
 import { NewTransactionModal } from './components/NewTransactionModal';
 
 import { GlobalStyle } from "./styles/global";
+import { TransactionsContext } from './TransactionsContext';
 
 Modal.setAppElement('#root');
 
@@ -21,7 +22,7 @@ export function App() {
   }
 
   return (
-    <>
+    <TransactionsContext.Provider value={[]}>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
       <NewTransactionModal
@@ -30,6 +31,6 @@ export function App() {
       />
 
       <GlobalStyle />
-    </>
+    </TransactionsContext.Provider>
   );
 }
