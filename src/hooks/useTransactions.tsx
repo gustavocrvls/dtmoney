7fb4a6d8e5a9e6faa559c5c678fb94314b3d9d71
Gustavo Carvalho Silva <gustavocrvl42@gmail.com>
2021-05-24 22:34:46 -0300

    Criação de hook de transaction

--- src/hooks/useTransactions.tsx
+++ src/hooks/useTransactions.tsx
@@ -1,6 +1,6 @@
-import { createContext, ReactNode } from "react";
+import { createContext, ReactNode, useContext } from "react";
 import { useEffect, useState } from "react";
-import { api } from "./services/api";
+import { api } from "../services/api";
 
 interface Transaction {
   id: number;
@@ -47,4 +47,10 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
       {children}
     </ TransactionsContext.Provider>
   )
+}
+
+export function useTransactions() {
+  const context = useContext(TransactionsContext);
+
+  return context;
 }
